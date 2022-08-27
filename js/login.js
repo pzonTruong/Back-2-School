ecca6274b0a85c9705a657494b7220e48d0de2b3
P_Son <86548421+gitbubtezer@users.noreply.github.com>
2022-08-27 21:14:50 +0700

    change link color & update sign in and purchase button

--- js/login.js
+++ js/login.js
@@ -1,17 +1,22 @@
 const formlog = document.getElementById("login-form");
 const BtnLog = document.getElementById("login-form-submit");
 const ErrorMessage = document.getElementById("login-error-msg");
+var account = [["admin@gmail.com","12345"],["son@gmail.com","son12345"]]
 
 BtnLog.addEventListener("click", (e) => {
     e.preventDefault();
     const email = formlog.email.value;
     const password = formlog.password.value;
 
-    if (email === "admin@gmail.com" && password === "12345") {
-        alert("You have successfully logged in.");
-        window.location.replace("/Back-2-School/index.html");
-        //location.reload();
-    } else {
-        ErrorMessage.style.opacity = 1;
+    for(i=0;i<=account.length;i++){
+        if (email === account[i][0] && password === account[i][1]) {
+            alert("You have successfully logged in.");
+            window.location.replace("/Back-2-School/index.html");
+            //location.reload();
+        } 
     }
 })
+
+
+
+
---
+++ js/purchase.js
@@ -0,0 +1,5 @@
+const Btn = document.getElementById("buy");
+
+Btn.addEventListener("click", (e) => {
+    window.location.replace("/Back-2-School/web/purchase.html");
+})
